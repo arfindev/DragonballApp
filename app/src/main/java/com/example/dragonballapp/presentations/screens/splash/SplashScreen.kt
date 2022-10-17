d1c206e31d1e812493335c76586c1f350bd4cca5
Arfin Hosain <developerarfin@gmail.com>
2022-10-17 08:57:53 +0600

    Splash Screen Animation Added

--- app/src/main/java/com/example/dragonballapp/presentations/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/dragonballapp/presentations/screens/splash/SplashScreen.kt
@@ -1,14 +1,17 @@
 package com.example.dragonballapp.presentations.screens.splash
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -19,13 +22,23 @@ import com.example.dragonballapp.ui.theme.DarkRed
 import com.example.dragonballapp.ui.theme.Yellow
 
 @Composable
-fun SplashScreen(navHostController: NavHostController) {
-    Splash()
+fun SplashScreen(navHostController: NavHostController, pulseFraction: Float = 1.2f) {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = pulseFraction,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Splash(scale = scale)
 }
 
 
 @Composable
-fun Splash() {
+fun Splash(scale: Float) {
     if (isSystemInDarkTheme()) {
         Box(
             modifier = Modifier
@@ -34,6 +47,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.scale(scale),
                 painter = painterResource(id = R.drawable.splash_screen),
                 contentDescription = "Splash Screen"
             )
@@ -47,6 +61,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.scale(scale),
                 painter = painterResource(id = R.drawable.splash_screen),
                 contentDescription = "Splash Screen"
             )
@@ -60,13 +75,13 @@ fun Splash() {
 @Preview
 @Composable
 fun PreviewSplashScreen() {
-    Splash()
+    Splash(scale = 1f)
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewDarkSplashScreen() {
-     Splash()
+    Splash(scale = 1f)
 
 
 }
